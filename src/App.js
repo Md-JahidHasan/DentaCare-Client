ff3f6dc9acf1865c44a5c40fb45262ad01ea226f
MdJahid-Hasan <md.jahidhasan.tex@gmail.com>
2022-11-09 19:17:14 +0600

    service wise review load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { firebaseConfig } from './firebase.config';
 import {RouterProvider } from 'react-router-dom';
 import router from './components/Routes/Router';
 import 'react-photo-view/dist/react-photo-view.css';
 
-
 function App() {
+  
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
